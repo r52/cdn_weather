e21f1b00d21d3073dd243f20e65845c5a8f11a84
Simon <homer12345@gmail.com>
2018-01-17 22:15:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### cdn_weather
+
+A widget for use with [Quasar](https://github.com/r52/quasar) (or anything else that works).
+
+Displays up-to-date weather information for major Canadian locations.
+
+Configurations available in the js/options.js file.
---
+++ js/options.js
@@ -0,0 +1,29 @@
+// 2 letter provincial code
+// The code is one of the 13 following values:
+// ** AB (Alberta)
+// ** BC (British Columbia)
+// ** MB (Manitoba)
+// ** NB (New Brunswick)
+// ** NL (Newfoundland and Labrador)
+// ** NS (Nova Scotia)
+// ** NT (Northwest Territories)
+// ** NU (Nunavut)
+// ** ON (Ontario)
+// ** PE (Prince Edward Island)
+// ** QC (Québec)
+// ** SK (Saskatchewan)
+// ** YT (Yukon)
+
+var ProvCode = "";
+
+// SiteNameCode: code corresponding to the site codes used in the city site list
+// Lists with location names and and SiteNameCodes, regrouped according to
+// various factors, can be found at the following addresses:
+// http://dd.weather.gc.ca/citypage_weather/docs/site_list_en.csv
+// http://dd.weather.gc.ca/citypage_weather/docs/site_list_provinces_en.csv
+// http://dd.weather.gc.ca/citypage_weather/docs/site_list_towns_en.csv
+var SiteNameCode = "";
+
+// single letter indicating the language of the file. Can be either: f
+// (French) or e (English)
+var lang = "e";
